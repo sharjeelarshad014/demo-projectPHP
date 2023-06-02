67c72c1a5755241665fcd1c60b7e125cc7faf58b
sharjeelarshad014 <46492824+sharjeelarshad014@users.noreply.github.com>
2023-06-02 05:35:16 +0500

    new homew

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 	<?php include("./includes/brand-info.php"); ?>
-	<title>wvikki home | <?php echo "$brandName";?></title>
+	<title>new home | <?php echo "$brandName";?></title>
 	<meta name="description" content="">
 	<meta name="keywords" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
