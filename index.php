c3098f87794031b9ce3ec2b31bcfb864f9a46e6b
sharjeelarshad014 <46492824+sharjeelarshad014@users.noreply.github.com>
2023-06-02 05:17:44 +0500

    homee

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 	<?php include("./includes/brand-info.php"); ?>
-	<title>Home | <?php echo "$brandName";?></title>
+	<title>Homee | <?php echo "$brandName";?></title>
 	<meta name="description" content="">
 	<meta name="keywords" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
